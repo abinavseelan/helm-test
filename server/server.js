54838dcae5302f041726c734b4d0a329364f57a8
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-11-09 20:25:47 +0530

    helm done

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => {
-  return res.status(200).send('Pong! 🏓')
+app.get("/ping", (req, res) => {
+  return res.status(200).send("Pong! 🏓");
 });
 
+app.get("/cat", (_req, res) =>
+   res.send({ nodeEnv: process.env.NODE_ENV, catName: process.env.CAT_NAME })
+);
+
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
